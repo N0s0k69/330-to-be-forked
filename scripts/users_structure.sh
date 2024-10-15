404b42f3155ea47e8e0debb5267f9e9aea4e63ce
N0s0k69 <halyvasires@gmail.com>
2024-10-15 17:10:04 +0700

    1

---
+++ scripts/users_structure.sh
@@ -0,0 +1,30 @@
+read -p "imy? " USERNAME
+mkdir ~/$USERNAME
+git init ~/$USERNAME/dev
+
+mkdir -p ~/$USERNAME/dev/photos/{2020,2021,2022,2023,2024}
+touch ~/$USERNAME/dev/photos/2020/default.png
+touch ~/$USERNAME/dev/photos/2021/default.png
+touch ~/$USERNAME/dev/photos/2022/default.png
+touch ~/$USERNAME/dev/photos/2023/default.png
+touch ~/$USERNAME/dev/photos/2024/default.png
+git add .
+git commit -m "1 "
+
+mkdir -p ~/$USERNAME/dev/videos/{2020,2021,2022,2023,2024}
+touch ~/$USERNAME/dev/videos/2020/default.mp4
+touch ~/$USERNAME/dev/videos/2021/default.mp4
+touch ~/$USERNAME/dev/videos/2022/default.mp4
+touch ~/$USERNAME/dev/videos/2023/default.mp4
+touch ~/$USERNAME/dev/videos/2024/default.mp4
+git add .
+git commit -m "2 "
+
+mkdir -p ~/$USERNAME/dev/documents/{2020,2021,2022,2023,2024}
+touch ~/$USERNAME/dev/documents/2020/default.mp4
+touch ~/$USERNAME/dev/documents/2021/default.mp4
+touch ~/$USERNAME/dev/documents/2022/default.mp4
+touch ~/$USERNAME/dev/documents/2023/default.mp4
+touch ~/$USERNAME/dev/documents/2024/default.mp4
+git add .
+git commit -m "3 "
